9feacd39fcd1e261292e221ad57b09b30351e0c8
Iftekhar Bhuiyan <ibhuiyan@mail.com>
2024-04-25 19:14:08 -0400

    formatting update

--- functions.php
+++ functions.php
@@ -1,30 +1,29 @@
 <?php
 /**
- * display a list of random posts
+ * Display a list of random posts
  * on single post page in WordPress
- * @param int: number of posts
- * @return str: list of posts
+ * @param int $post_count
+ * @return string $data
  */
 function get_random_posts($post_count) {
-	if (is_numeric($post_count)) {
-		global $post;
-		// variable for string
-		$data = '';
-		// setup parameters
-		$args = array(
-			'post_type' => 'post',
-			'posts_per_page' => $post_count,
-			'orderby' => 'rand'
-		);
-		// query results
-		$random_posts = get_posts($args);
-		$data = '<ul>';
-		// loop through posts
-		foreach ($random_posts as $post) {
-			$data .= '<li><a href="'.get_the_permalink().'">'.get_the_title().'</a></li>';
-		}
-		$data .= '</ul>';
-		return $data;
-	}
+    if (is_numeric($post_count)) {
+        global $post;
+        // variable for string
+        $data = '';
+        // arguments
+        $args = array(
+            'post_type' => 'post',
+            'posts_per_page' => $post_count,
+            'orderby' => 'rand'
+        );
+        // query posts
+        $random_posts = get_posts($args);
+        $data = '<ul>';
+        foreach ($random_posts as $post) {
+            $data .= '<li><a href="'.get_the_permalink().'">'.get_the_title().'</a></li>';
+        }
+        $data .= '</ul>';
+        return $data;
+    }
 }
 ?>
--- single.php
+++ single.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * default post template
+ * Default post template
  * snippet will go inside default loop
  */
 if (have_posts()) {
-	while (have_posts()) {
-		the_post();
-		// get list of random posts
-		if (function_exists('get_random_posts')) {
-			echo get_random_posts('2');
-		}
-	}
+    while (have_posts()) {
+        the_post();
+        // get list of random posts
+        if (function_exists('get_random_posts')) {
+            echo get_random_posts('2');
+        }
+    }
 }
 ?>
